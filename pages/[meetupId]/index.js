a3d0e516c7b692e35b5637b9e1501158dc824484
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-02-09 19:35:30 +0530

    getserversideProps added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 const MeetupDetailPage = () => {
   return (
@@ -11,4 +12,44 @@ const MeetupDetailPage = () => {
     />
   );
 };
-export default MeetupDetailPage
+
+export async function getStaticPaths() {
+  return {
+    fallback: true,
+    paths: [
+      {
+        params: {
+          meetupId: "M1",
+        },
+      },
+      {
+        params: {
+          meetupId: "M2",
+        },
+      },
+      {
+        params: {
+          meetupId: "M2",
+        },
+      },
+    ],
+  };
+}
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  //fetch actually
+  return {
+    props: {
+      meetupDetail: {
+        id: "m1",
+        title: "First Meetup",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
+        address: "First Meetup address",
+        description: "this is first meetups",
+      },
+    },
+  };
+}
+export default MeetupDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
-import MeetupList from "../components/meetups/MeetupList"
+import { useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
 const DUMMY_MEETUPS = [
-    {
-        id:"M1",
-        title:"First Meetup",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
-        address:"First Meetup address",
-        description:"this is first meetups"
-    },
-    {
-        id:"M2",
-        title:"Second Meetup",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
-        address:"Second Meetup address",
-        description:"this is Second meetups"
-    },
-    {
-        id:"M3",
-        title:"Third Meetup",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
-        address:"Third Meetup address",
-        description:"this is Third meetups"
-    }
-]
+  {
+    id: "M1",
+    title: "First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
+    address: "First Meetup address",
+    description: "this is first meetups",
+  },
+  {
+    id: "M2",
+    title: "Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
+    address: "Second Meetup address",
+    description: "this is Second meetups",
+  },
+  {
+    id: "M3",
+    title: "Third Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Qutub_Minar_932.jpg/240px-Qutub_Minar_932.jpg",
+    address: "Third Meetup address",
+    description: "this is Third meetups",
+  },
+];
 const HomePage = () => {
-  return (
-    <MeetupList meetups={DUMMY_MEETUPS}/>
-  )
-}
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
 
-export default HomePage
+export async function getStaticProps() { //it runs on building the prod build before deploying
+  //fetch data originally
+  return {
+    props: {
+      // using props as a name to return is mandatory
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10, // it is used check for the data every 10 second even so that u dont need to deplaoy it everytime the data changes
+  };
+}
+export default HomePage;
